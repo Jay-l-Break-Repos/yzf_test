a4b6113c73e27d8c9bad934738cebee05a9c52ce
zhifei yu <yuzhifei0102@gmail.com>
2026-03-05 22:54:05 -0500

    Delete s-S01-samplerepo1-UA0000S01/s-S01-samplerepo1-UA0000S01 directory

--- s-S01-samplerepo1-UA0000S01/s-S01-samplerepo1-UA0000S01/src/backend/app/models/user.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-from sqlmodel import SQLModel, Field
-
-class User(SQLModel, table=True):
-    __tablename__ = "users"
-    id: Optional[int] = Field(default=None, primary_key=True)
-    browser_id: str = Field(index=True, unique=True)
-    nickname: str = Field(default="Anonymous")
--- s-S01-samplerepo1-UA0000S01/s-S01-samplerepo1-UA0000S01/src/frontend/src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './CustomAlert';
-export * from './Sidebar';
